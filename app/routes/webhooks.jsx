18b61ef2f0461520ed021b47202a4b56144cc809
Sandeep Singh <sandy1creation@gmail.com>
2025-12-03 10:17:22 +0530

    solve

--- app/routes/webhooks.jsx
+++ app/routes/webhooks.jsx
@@ -1,5 +1,4 @@
 import { authenticate } from "../shopify.server";
-import db from "../db.server";
 
 export const action = async ({ request }) => {
   const { shop, session, topic } = await authenticate.webhook(request);
